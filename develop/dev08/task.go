c4ea0c59469bea9724eee3733f8d4378b168e143
satanaroom <oficialsatana@mail.ru>
2022-04-07 21:41:44 +0300

    dev08 done

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
 /*
 === Взаимодействие с ОС ===
 
@@ -14,6 +25,74 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+const (
+	commandError = `error command occurred: the following commands are supported:
+	$ cd   — смена директории
+	$ pwd  — вывод текущей директории
+	$ echo — вывод строки текста
+	$ kill — убить процесс
+	$ ps   — вывод списка процессов`
+	ShellToUse    = "bash"
+	MyShellStart  = `======================Welcome to MyShell 1.0======================`
+	MyShellFinish = `==================================================================`
+)
 
+func Shellout(command string, cd *Cd) (error, string) {
+	var stdout bytes.Buffer
+	cmd := exec.Command(ShellToUse, "-c", command)
+	cmd.Dir = cd.dirName
+	cmd.Stdout = &stdout
+	err := cmd.Run()
+	return err, stdout.String()
+}
+
+func parseArgs(cmd string, cd *Cd) {
+	var wasCd bool
+	var res []string
+	res = strings.Split(cmd, " ")
+	for _, val := range res {
+		if val != "cd" && !wasCd {
+			continue
+		}
+		if !wasCd {
+			wasCd = true
+			continue
+		}
+		fileInfo, _ := os.Stat(val)
+		if fileInfo.IsDir() {
+			cd.dirName = val
+		}
+		break
+	}
+}
+
+type Cd struct {
+	dirName string
+}
+
+func main() {
+	cd := &Cd{}
+	sc := bufio.NewScanner(os.Stdin)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT)
+	fmt.Println(MyShellStart)
+	go func() {
+		for {
+			if sc.Scan() {
+				cmd := sc.Text()
+				parseArgs(cmd, cd)
+				err, out := Shellout(cmd, cd)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s\n", err)
+					os.Exit(1)
+				}
+				fmt.Printf("%s", out)
+			}
+		}
+	}()
+	select {
+	case <-sigChan:
+		fmt.Println(MyShellFinish)
+		os.Exit(0)
+	}
 }
