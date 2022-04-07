a5074820438d4e001ab0321b38aa7efe97a04c8d
satanaroom <oficialsatana@mail.ru>
2022-04-07 17:17:12 +0300

    dev07 done

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,19 +1,94 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
-=== Взаимодействие с ОС ===
+=== Or channel ===
 
-Необходимо реализовать собственный шелл
+Реализовать функцию, которая будет объединять один или более done каналов в single канал если один из его составляющих каналов закроется.
+Одним из вариантов было бы очевидно написать выражение при помощи select, которое бы реализовывало эту связь,
+однако иногда неизестно общее число done каналов, с которыми вы работаете в рантайме.
+В этом случае удобнее использовать вызов единственной функции, которая, приняв на вход один или более or каналов, реализовывала весь функционал.
 
-встроенные команды: cd/pwd/echo/kill/ps
-поддержать fork/exec команды
-конвеер на пайпах
+Определение функции:
+var or func(channels ...<- chan interface{}) <- chan interface{}
 
-Реализовать утилиту netcat (nc) клиент
-принимать данные из stdin и отправлять в соединение (tcp/udp)
-Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
+Пример использования функции:
+sig := func(after time.Duration) <- chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		time.Sleep(after)
+}()
+return c
+}
+
+start := time.Now()
+<-or (
+	sig(2*time.Hour),
+	sig(5*time.Minute),
+	sig(1*time.Second),
+	sig(1*time.Hour),
+	sig(1*time.Minute),
+)
+
+fmt.Printf(“fone after %v”, time.Since(start))
 */
 
-func main() {
+// Fan-In паттерн
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	var wg sync.WaitGroup
 
+	out := make(chan interface{})
+
+	// Запускаем send goroutine
+	// для каждого входящего канала в cs.
+	// send копирует значения из c в out
+	// до тех пор пока c не закрыт, затем вызываем wg.Done.
+	send := func(c <-chan interface{}) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+
+	wg.Add(len(channels))
+	for _, c := range channels {
+		go send(c)
+	}
+
+	// Запускаем goroutine чтобы закрыть out
+	// когда все send goroutine выполнены
+	// Это должно начаться после вызова wg.Add.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Second),
+		sig(5*time.Second),
+		sig(1*time.Second),
+		sig(3*time.Second),
+		sig(4*time.Second),
+	)
+
+	fmt.Printf("done after %v", time.Since(start))
 }
